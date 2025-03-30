b1a9941fbeecea872317f3811d405864e9218ee3
lpv2912004 <lamphuocvinh2@gmail,com>
2025-03-30 14:01:51 +0700

    design header for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import './App.css';
-import MainLayout from '@components/Layout/Layout';
 import MyHeader from '@components/Header/Header';
-import MyFooter from '@components/Footer/Footer';
-import MyButton from '@components/Button/Button';
 
 function App() {
     return (
         <>
-            <MainLayout>
-                {/* <MyHeader />
-                Content
-                <MyFooter /> */}
-                <MyButton />
-            </MainLayout>
+            <MyHeader />
         </>
     );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,80 @@
+import BoxIcon from './BoxIcon/BoxIcon';
+import { dataBoxIcon, dataMenu } from './Constant';
+import Menu from './Menu/Menu';
+import styles from './styles.module.scss';
+import Logo from '@icons/images/Logo-retina.png';
+import CartIcon from '@icons/svgs/cartIcon.svg';
+import HeartIcon from '@icons/svgs/heartIcon.svg';
+import ReloadIcon from '@icons/svgs/reloadIcon.svg';
 function MyHeader() {
-    return <div>My Header</div>;
+    const {
+        containerHeader,
+        containerBox,
+        containerBoxIcon,
+        containerMenu,
+        container
+    } = styles;
+    return (
+        <div className={container}>
+            <div className={containerHeader}>
+                <div className={containerBox}>
+                    <div className={containerBoxIcon}>
+                        {dataBoxIcon.map((item) => {
+                            return (
+                                <BoxIcon type={item.type} href={item.href} />
+                            );
+                        })}
+                    </div>
+                    <div className={containerMenu}>
+                        {dataMenu.slice(0, 3).map((item) => {
+                            return (
+                                <Menu content={item.content} href={item.href} />
+                            );
+                        })}
+                    </div>
+                </div>
+                <div>
+                    <img
+                        style={({ width: '153px' }, { height: '53px' })}
+                        src={Logo}
+                        alt='Logo'
+                    />
+                </div>
+
+                <div className={containerBox}>
+                    <div className={containerMenu}>
+                        {dataMenu.slice(3, dataMenu.length).map((item) => {
+                            return (
+                                <Menu content={item.content} href={item.href} />
+                            );
+                        })}
+                    </div>
+                    <div className={containerBoxIcon}>
+                        <img
+                            width={26}
+                            height={26}
+                            src={ReloadIcon}
+                            alt='reloadIcon'
+                        />
+
+                        <img
+                            width={26}
+                            height={26}
+                            src={HeartIcon}
+                            alt='heartIcon'
+                        />
+
+                        <img
+                            width={26}
+                            height={26}
+                            src={CartIcon}
+                            alt='cartIcon'
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default MyHeader;
