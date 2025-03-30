f57ec9707b75dae510a9cd8ec3e25145b7912780
lpv2912004 <lamphuocvinh2@gmail,com>
2025-03-30 08:54:39 +0700

    add file setup formatter pretteir

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-     <div>hello world</div>
-    </>
-  )
+    const file = (x) => {
+        console.log(x);
+    };
+
+    return (
+        <>
+            <div>hello world</div>
+        </>
+    );
 }
 
-export default App
+export default App;
