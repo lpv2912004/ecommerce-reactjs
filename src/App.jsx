22f5619afd9a9e0f48bd3106ec5765e58069673b
lpv2912004 <lamphuocvinh2@gmail,com>
2025-04-03 09:47:47 +0700

    Add Router Dom For Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import Blog from '@components/Blog/Blog';
 import HomePage from '@components/HomePage/HomePage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import routers from '@/router/routers';
+import { Suspense } from 'react';
 function App() {
     return (
         <>
-            <HomePage />
+            <BrowserRouter>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Routes>
+                        {routers.map((item, index) => {
+                            return <Route path={item.path} element={<item.component />} />;
+                        })}
+                    </Routes>
+                </Suspense>
+            </BrowserRouter>
         </>
     );
 }
